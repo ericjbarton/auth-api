a30922648fe6e00f41ab39448ccd084aef3d0c0b
ericjbarton <erockbart@gmail.com>
2022-07-15 08:11:41 -0400

    adds users controller, create method and routes, seed data

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+  end
+end
